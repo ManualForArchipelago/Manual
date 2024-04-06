e7d7a2d2e72f3498935195f44801ced434be0fa7
nicopop <6759630+nicopop@users.noreply.github.com>
2024-04-05 21:33:41 -0400

    correct crash if no bug_report_page by default

--- src/Meta.py
+++ src/Meta.py
@@ -22,7 +22,10 @@ def set_world_webworld(web: WebWorld) -> WebWorld:
         web.game_info_languages = Web_Config.get("game_info_languages", web.game_info_languages)
         web.options_presets = Web_Config.get("options_presets", web.options_presets)
         web.options_page = Web_Config.get("options_page", web.options_page)
-        web.bug_report_page = Web_Config.get("bug_report_page", web.bug_report_page)
+        if hasattr(web, 'bug_report_page'):
+            web.bug_report_page = Web_Config.get("bug_report_page", web.bug_report_page)
+        else:
+            web.bug_report_page = Web_Config.get("bug_report_page", None)
 
         if Web_Config.get("tutorials", []):
             tutorials = []
