145e71db6988ca835c1f8686abbc7075443f9720
Katelyn Gigante <clockwork.singularity@gmail.com>
2024-04-18 21:20:52 +1000

    Ability to mark locations as always pre-hinted

--- src/Regions.py
+++ src/Regions.py
@@ -1,7 +1,7 @@
 from BaseClasses import Entrance, MultiWorld, Region
 from .Helpers import is_category_enabled, is_location_enabled
 from .Data import region_table
-from .Locations import ManualLocation
+from .Locations import ManualLocation, location_name_to_location
 from worlds.AutoWorld import World
 from .hooks.Regions import before_region_table_processed
 
@@ -61,6 +61,8 @@ def create_region(world: World, multiworld: MultiWorld, player: int, name: str, 
         for location in locations:
             loc_id = world.location_name_to_id.get(location, 0)
             locationObj = ManualLocation(player, location, loc_id, ret)
+            if location_name_to_location[location].get('prehint'):
+                multiworld.start_location_hints[player].value.add(location)
             ret.locations.append(locationObj)
     if exits:
         for exit in exits:
