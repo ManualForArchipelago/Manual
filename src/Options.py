3192f043e092250467de98475a3f1be4591b8e15
nicopop <6759630+nicopop@users.noreply.github.com>
2024-05-29 21:30:46 -0400

    made the options generation actually work

--- src/Options.py
+++ src/Options.py
@@ -19,18 +19,16 @@ def convertToLongString(input: str | list) -> str: #Todo maybe find a better nam
 
 def createChoiceOptions(values: dict, aliases: dict) -> dict:
     values = {'option_' + i: v for i, v in values.items()}
-    aliases += {'alias_' + i: v for i, v in aliases.items()}
+    aliases = {'alias_' + i: v for i, v in aliases.items()}
     return {**values, **aliases}
 
 manual_options = before_options_defined({})
 
-for option in option_table:
-    option_name = option['name']
+for option_name, option in option_table.items():
     if option_name.startswith('_'): #To allow commenting out options
         continue
     option_type = Toggle
-    args = {'display_name': option.get('display_name', option_name),
-            __doc__ : convertToLongString(option.get('description', ""))}
+    args = {'display_name': option.get('display_name', option_name)}
     if option['type'] == "DefaultOnToggle":
         option_type = DefaultOnToggle
     elif option['type'] == "Choice" or option['type'] == "TextChoice":
@@ -45,6 +43,7 @@ for option in option_table:
         continue
     if option_name not in manual_options:
         manual_options[option_name] = type(option_name, (option_type,), args )
+        manual_options[option_name].__doc__ = convertToLongString(option.get('description', "an Option"))
 
 if len(victory_names) > 1:
     goal = {'option_' + v: i for i, v in enumerate(victory_names)}
