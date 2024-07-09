28de3e1bbfa52fb36af884eeeaa8cc426dae923c
Katelyn Gigante <clockwork.singularity@gmail.com>
2024-07-10 02:14:08 +1000

    Bump client version number

--- src/__init__.py
+++ src/__init__.py
@@ -409,7 +409,7 @@ class VersionedComponent(Component):
         self.version = version
 
 def add_client_to_launcher() -> None:
-    version = 2024_05_05 # YYYYMMDD
+    version = 2024_07_10 # YYYYMMDD
     found = False
     for c in components:
         if c.display_name == "Manual Client":
