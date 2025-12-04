3ec8f29a2e26ae5b2e1022cca57d5265a57666aa
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-12-04 17:01:07 +1100

    Ignore "The " when applying natural sorting

--- src/ManualClient.py
+++ src/ManualClient.py
@@ -629,11 +629,16 @@ class ManualContext(SuperContext):
 
                 elif abs(loc_sorting) == SortingOrderLoc.natural:
                     # Modified from https://stackoverflow.com/a/11150413
-                    convert = lambda text: int(text) if text.isdigit() else text.lower()
-                    alphanum_key = lambda i: [
-                                convert(c) for c in re.split('([0-9]+)', \
-                                self.ctx.get_location_by_id(i).get("sort-key", self.ctx.get_location_by_id(i).get("name", "")))
-                            ]
+                    def convert(text):
+                        return int(text) if text.isdigit() else text.lower()
+
+                    def alphanum_key(i):
+                        name = self.ctx.get_location_by_id(i).get("name", "")
+                        if name.lower().startswith("the "):
+                            name = name[4:]  # remove leading "The " for sorting purposes
+
+                        return [convert(c) for c in re.split('([0-9]+)', self.ctx.get_location_by_id(i).get("sort-key", name))]
+
                     for category in self.listed_locations:
                         self.listed_locations[category].sort(key=alphanum_key, reverse=loc_sorting < 0)
 
@@ -810,13 +815,16 @@ class ManualContext(SuperContext):
                                     reverse=item_sorting < 0)
 
                                 elif abs(item_sorting) == SortingOrderItem.natural:
-                                    convert = lambda text: int(text) if text.isdigit() else text.lower()
-                                    alphanum_key = lambda i: [
-                                                convert(c) for c in re.split('([0-9]+)', \
-                                                self.ctx.get_item_by_id(i).get("sort-key", self.ctx.get_item_by_id(i).get("name", "")))
-                                            ]
-                                    sorted_items_received = sorted(sorted_items_received,
-                                    key=alphanum_key, reverse=item_sorting < 0)
+                                    def convert(text):
+                                        return int(text) if text.isdigit() else text.lower()
+                                    def alphanum_key(i):
+                                        name = self.ctx.get_item_by_id(i).get("name", "")
+                                        if name.lower().startswith("the "):
+                                            name = name[4:]  # remove leading "The " for sorting purposes
+
+                                        return [convert(c) for c in re.split('([0-9]+)',self.ctx.get_item_by_id(i).get("sort-key", name))
+                                                                                ]
+                                    sorted_items_received = sorted(sorted_items_received, key=alphanum_key, reverse=item_sorting < 0)
 
                                 elif abs(item_sorting) == SortingOrderItem.received:
                                     if item_sorting < 0:
