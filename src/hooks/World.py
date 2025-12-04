0a367cc042871851d87ad14957d57458566c563e
Katelyn Gigante <clockwork.singularity@gmail.com>
2025-12-04 11:11:29 +1100

    Add before_generate_early hook (#178)

--- src/hooks/World.py
+++ src/hooks/World.py
@@ -36,6 +36,13 @@ import logging
 def hook_get_filler_item_name(world: World, multiworld: MultiWorld, player: int) -> str | bool:
     return False
 
+def before_generate_early(world: World, multiworld: MultiWorld, player: int) -> None:
+    """
+    This is the earliest hook called during generation, before anything else is done.
+    Use it to check or modify incompatible options, or to set up variables for later use.
+    """
+    pass
+
 # Called before regions and locations are created. Not clear why you'd want this, but it's here. Victory location is included, but Victory event is not placed yet.
 def before_create_regions(world: World, multiworld: MultiWorld, player: int):
     pass
