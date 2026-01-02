2d1587f5573d0b77d1e32a87e74a5901fb8cba47
Katelyn Gigante <clockwork.singularity@gmail.com>
2026-01-02 15:52:36 +1100

    Fix indentation for new code

--- src/__init__.py
+++ src/__init__.py
@@ -283,8 +283,8 @@ class ManualWorld(World):
         else:
             classification = ItemClassification.filler
 
-        if "classification" in item and item["classification"]:
-            classification = reduce((lambda a, b: a | b), {ItemClassification[str_classification.strip()] for str_classification in item["classification"].split(",")})
+            if "classification" in item and item["classification"]:
+                classification = reduce((lambda a, b: a | b), {ItemClassification[str_classification.strip()] for str_classification in item["classification"].split(",")})
 
             if "trap" in item and item["trap"]:
                 classification |= ItemClassification.trap
