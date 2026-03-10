332189a2b1fecd2ac01419b948f7037d2dd17ad4
nicopop <6759630+nicopop@users.noreply.github.com>
2026-03-10 13:30:55 -0400

    fix before_is_event_enabled hook description and parameter name

--- src/hooks/Helpers.py
+++ src/hooks/Helpers.py
@@ -18,6 +18,6 @@ def before_is_location_enabled(multiworld: MultiWorld, player: int, location:  d
     return None
 
 # Use this if you want to override the default behavior of is_option_enabled
-# Return True to enable the location, False to disable it, or None to use the default behavior
-def before_is_event_enabled(multiworld: MultiWorld, player: int, location:  dict[str, Any]) -> Optional[bool]:
+# Return True to enable the event, False to disable it, or None to use the default behavior
+def before_is_event_enabled(multiworld: MultiWorld, player: int, event:  dict[str, Any]) -> Optional[bool]:
     return None
