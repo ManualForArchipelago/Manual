9913d3c31a9c4aa9d88f913582a84c8d7537ceae
Julien Forget <julien.forget@calculquebec.ca>
2026-04-22 23:38:14 -0400

    Updated requirements for victory location

--- manual_KirbyMassAttack.py
+++ manual_KirbyMassAttack.py
@@ -1,6 +1,4 @@
 import json
-import pandas as pd
-
 GG_medals = {1:5,4:5,5:5,6:5,7:5,8:5,10:5,
                2:3,3:3,9:3,
              11:0}
@@ -35,14 +33,103 @@ mapping_VV = {1:"Outer Ring",2:"Outer Ring",3:"Outer Ring",
               4:"Middle Ring",5:"Middle Ring",6:"Middle Ring",7:"Middle Ring",
               8:"Inner Ring",9:"Inner Ring",10:"Inner Ring",11:"Inner Ring",
               12:"Boss"}
-mappings = {"GG":mapping_GG, "SC":mapping_SC, "DR":mapping_DR,"VV":mapping_VV}
+mapping_NN = {1:"Outer Ring", 2:"Outer Ring", 3:"Outer Ring", 4:"Outer Ring",
+              5:"Boss"}
+mappings = {"GG":mapping_GG, "SC":mapping_SC, "DR":mapping_DR,"VV":mapping_VV, "NN":mapping_NN}
 mappings_medal = {"GG":GG_medals, "SC":SC_medals, "DR":DR_medals,"VV":VV_medals}
 
 
+def requirements_completion(stage_number, world_name, medal_number=0):
+    requirements = ""
+    if world_name == "Green Grounds":
+        if stage_number == 1:
+            requirements += "|Bubble Ability| and |Bomb Block|"
+            if medal_number == 3:
+                requirements += " and |Extra Kirby:5|"
+            if medal_number == 0 or medal_number == 5:
+                requirements += " and |Cannon|"
+        if stage_number == 2:
+            requirements += "|Melody Switch|"
+            if medal_number ==3:
+                requirements += " and |Keys|"
+        if stage_number == 3:
+            requirements += "|Cannon|"
+            if medal_number ==2:
+                requirements += " and |Extra Kirby:10|"
+        if stage_number == 4:
+            if medal_number == 1:
+                requirements += "|Keys|"
+            if medal_number == 2:
+                requirements += "|Bubble Ability|"
+            if medal_number == 4:
+                requirements += "|Cannon| and |Extra Kirby:10|"
+            if medal_number == 0:
+                requirements += "!Cannon!"
+
+    if world_name == "Sandy Canyon":
+        if stage_number == 1:
+            requirements += "|Bomb Block|"
+        if stage_number == 2:
+            if medal_number ==2:
+                requirements += "|Extra Kirby:10|"
+            if medal_number ==3:
+                requirements += "|Cannon|"
+        if stage_number == 3:
+            requirements += "|Melody Switch|"
+            if medal_number == 2:
+                requirements += " and |Destroyable Brown Block|"
+            if medal_number == 3:
+                requirements += " and |Cannon|"
+            if medal_number == 4 :
+                requirements += " and |Extra Kirby:10|"
+            if medal_number == 5 :
+                requirements += " and |Bomb block| and |Skull Keys|"
+        if stage_number == 4:
+            if medal_number >= 3 :
+                requirements += "|Cannon|"
+            if medal_number == 3:
+                requirements += " and |Extra Kirby:10|"
+            if medal_number == 4:
+                requirements += " and |Destroyable Brown Block| and |Keys|"
+            if medal_number == 2:
+                requirements += "|Bomb Block|"
+            if medal_number == 0:
+                requirements += "(|Bomb Block| and |Melody Switch|) or |Cannon|"
+        if stage_number == 5:
+            requirements += "|Bomb Block|"
+            if medal_number == 1:
+                requirements += " and |Keys|"
+        if stage_number == 6:
+            requirements += "|Destroyable Brown Block|"
+            if medal_number >=3 or medal_number == 0:
+                requirements+= " and |Normal Switch|"
+            if medal_number >= 4 :
+                requirements += " and |Extra Kirby:10|"
+        if stage_number == 7:
+            if medal_number == 1:
+                requirements += "|Extra Kirby:10|"
+            if medal_number == 2:
+                requirements += "|Jumbo Candy|"
+            if medal_number == 3:
+                requirements += "|Skull Keys|"
+            if medal_number == 4 or medal_number == 5 or medal_number == 0:
+                requirements+= "|Melody Switch| and |Destroyable Brown Block|"
+            if medal_number == 5:
+                requirements += " and |Keys| and |Extra Kirby:10|"
+
+
+
+    return requirements
+
+import pandas as pd
 doc = pd.read_excel("Kirby Mass Attack Requirements.ods",0)
 doc1 = pd.DataFrame(pd.read_excel("Kirby Mass Attack Requirements.ods",1))
+print(doc1)
+print(type(doc1))
 doc1["World"] = doc1["World"].ffill()
+print(doc1)
 doc1["Stage"] = doc1["Stage"].ffill()
+print(doc1)
 doc1.fillna(0, inplace=True)
 
 def template_completion(stage_number, world_name, doc):
@@ -54,6 +141,7 @@ def template_completion(stage_number, world_name, doc):
 def template_medal(medal_number, stage_number, world_name, doc, doc1):
     requirements = f"|Kirby:{doc.loc[(doc['World'] == world_name) & (doc['Stage'] == stage_number)].iloc[0]["Kirbys"]}|"
     row = doc1.loc[(doc1['World'] == world_name) & (doc1['Stage'] == stage_number) & (doc1['Medal'] == medal_number)]
+    print(row)
     if row.iloc[0]["Kirbys"]:
         requirements += f" and |Kirby:{int(row.iloc[0]["Kirbys"])}|"
     if row.iloc[0]["Keys"]:
@@ -85,6 +173,11 @@ for i in range(1, 13):
     add_medal_locations(locations, i, "Sandy Canyon", doc, doc1)
     add_medal_locations(locations, i, "Volcano Valley", doc, doc1)
 
+for i in range(1, 6):
+    locations.append(template_completion(i, "Necro Nebula", doc ))
+locations[-1]["victory"] = True
+locations[-1]["category"] = ["Necro Nebula"]
 
-with open("manual_kirbymassattack_simularbre/data/locations.json", "w") as f:
+
+with open("worlds/manual_kirbymassattack_simularbre/data/locations.json", "w") as f:
     json.dump(locations, f,indent=4)
