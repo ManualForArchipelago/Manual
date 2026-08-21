bf19be79e2fbb90ef80ee20a2f533c1e9108c8c1
nicopop <6759630+nicopop@users.noreply.github.com>
2026-08-21 17:37:07 -0400

    fix typing for filler_item_name in game.py that can now be a list

--- src/Game.py
+++ src/Game.py
@@ -1,9 +1,9 @@
 from .Data import game_table
 
 game_name: str = "Manual_%s_%s" % (game_table["game"], game_table["creator"])
-filler_item_name: str = game_table["filler_item_name"] if "filler_item_name" in game_table else "Filler"
+filler_item_name: str | list[str] = game_table["filler_item_name"] if "filler_item_name" in game_table else "Filler"
 glitches_item_name: str | None = game_table["glitches_item_name"] if "glitches_item_name" in game_table else None
-starting_items: list[dict]|None = game_table["starting_items"] if "starting_items" in game_table else None
+starting_items: list[dict] | None = game_table["starting_items"] if "starting_items" in game_table else None
 
 if "starting_index" in game_table:
     try:
