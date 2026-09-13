3b3e92f17951e01224c55f3a0e9c48c3c6864baf
nicopop <6759630+nicopop@users.noreply.github.com>
2026-09-13 14:39:24 -0400

    RB convert arguments for rules too

--- src/Rules.py
+++ src/Rules.py
@@ -204,6 +204,7 @@ def set_rules(world: "ManualWorld", multiworld: MultiWorld, player: int):
                         func = getattr(ns, name, None)
 
                     if func and inspect.isclass(func) and issubclass(func, rule_builder.rules.Rule):
+                        convert_req_function_args(None, func, func_args, area['name'], world)
                         rule_class = func
                         break
 
@@ -501,6 +502,10 @@ def convert_req_function_args(state: CollectionState | None, func, args: list[st
     index = -1
     for parameter in parameters.values():
         target_type = parameter.annotation
+        if "rule_builder.options.OptionFilter" in str(parameter):
+            # parameters >= this one are added by rule builder and do not need to be converted
+            break
+
         index += 1
         if target_type in knownParameters:
             if target_type in [World, 'ManualWorld']:
